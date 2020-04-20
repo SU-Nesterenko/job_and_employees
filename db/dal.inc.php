efb9c7979e1495664a2759ea387797eac00bb827
antonkornev <m5f100@yandex.com>
2020-04-20 18:59:55 +0500

    Корнев добавил фитрацию для страницы "вакансии"

--- db/dal.inc.php
+++ db/dal.inc.php
@@ -3,14 +3,14 @@
 //CRUD - Create Read Update Delete
 //Создание нового пользователя (Create)
 function DBCreateVacancy($Name,$Cena,$Opisanie,$Rabota) {
-	//Предотвращение SQL-инъекций
-	$Name=_DBEscString($Name);
-	$Opisanie=_DBEscString($Opisanie);
-	$Rabota=(int)($Rabota);
-	$Cena=(int)$Cena;
-	
-	//Выполнение запроса к БД
-	_DBQuery("
+    //Предотвращение SQL-инъекций
+    $Name=_DBEscString($Name);
+    $Opisanie=_DBEscString($Opisanie);
+    $Rabota=(int)($Rabota);
+    $Cena=(int)$Cena;
+
+    //Выполнение запроса к БД
+    _DBQuery("
 		INSERT INTO vacancy(Nazvanie,salary,duty,Opisanie)
 		VALUES ('$Name','$Cena','$Rabota','$Opisanie')
 	");
@@ -18,54 +18,84 @@ function DBCreateVacancy($Name,$Cena,$Opisanie,$Rabota) {
 
 //Получение одного пользователя (Read)
 function DBGetVacancyOne($id) {
-	//Предотвращение SQL-инъекций
-	$id=(int)$id;
-	//Выполнение запроса
-	return _DBGetQuery("SELECT * FROM vacancy WHERE ID=$id");
+    //Предотвращение SQL-инъекций
+    $id=(int)$id;
+    //Выполнение запроса
+    return _DBGetQuery("SELECT * FROM vacancy WHERE ID=$id");
 }
 
 //Получение списка пользователей (Read)
-function DBFetchVacancy($search_string,$sort,$dir,$s,$l) {
-	//Предотвращение SQL-инъекций
-	$search_string=_DBEscString($search_string);
-	$sort=(int)$sort;
-	$dir=_DBEscString($dir);
-	$s=(int)$s;
-	$l=(int)$l;	
-	
-	//Формирование запроса
-	$limit="LIMIT $s,$l";
-	
-	$where_like="";
-	if(trim($search_string)!="") {
-		$search_string=_DBEscString($search_string);
-		$where_like="WHERE Nazvanie LIKE \"%$search_string%\"";
-	}
+function DBFetchVacancy($search_string, $sort, $dir, $s, $l, $sal_from = null, $sal_to = null) {
 
-	$order="";
-	if(trim($sort)!="" && $dir!="") 
-		$order="ORDER BY ".((int)$sort+2)." $dir";	
-	
-	//Выполнение запроса
-	return _DBFetchQuery("SELECT * FROM vacancy $where_like $order $limit");
+    //Предотвращение SQL-инъекций
+    $search_string=_DBEscString($search_string);
+    $sort=(int)$sort;
+    $dir=_DBEscString($dir);
+    $s=(int)$s;
+    $l=(int)$l;
+
+    $sal_from=(int)$sal_from;
+    $sal_to=(int)$sal_to;
+
+    //Формирование запроса
+    $limit="LIMIT $s,$l";
+
+    $where_like="";
+    if(trim($search_string)!="") {
+
+        $search_string=_DBEscString($search_string);
+        $where_like="WHERE Nazvanie LIKE \"%$search_string%\"";
+    }
+
+    if ($sal_from && $sal_to){
+        if ($where_like == ""){
+            $where_like .=" WHERE salary BETWEEN $sal_from AND $sal_to";
+        }
+        if ($where_like != ""){
+            $where_like .=" AND salary BETWEEN $sal_from AND $sal_to";
+        }
+    }
+    elseif ($sal_from){
+        if ($where_like == ""){
+            $where_like .=" WHERE salary >= $sal_from";
+        }
+        if ($where_like != ""){
+            $where_like .=" AND salary >= $sal_from";
+        }
+    }
+    elseif ($sal_to){
+        if ($where_like == ""){
+            $where_like .=" WHERE salary <= $sal_to";
+        }
+        if ($where_like != ""){
+            $where_like .=" AND salary <= $sal_to";
+        }
+    }
+
+    $order="";
+    if(trim($sort)!="" && $dir!="")
+        $order="ORDER BY ".((int)$sort+2)." $dir";
+
+    //Выполнение запроса
+    return _DBFetchQuery("SELECT * FROM vacancy $where_like $order $limit");
 }
 
 //Подсчёт общего числа пользователей в базе (Read)
-function DBCountAllVacancy() { 
-	return _DBRowsCount(_DBQuery("SELECT * from vacancy"));
+function DBCountAllVacancy() {
+    return _DBRowsCount(_DBQuery("SELECT * from vacancy"));
 }
 
 //Редактирование элемента (Update)
 function DBUpdateVacancy($id,$Name,$Cena,$Opisanie,$Rabota) {
-	//Предотвращение SQL-инъекций
-	$id=(int)$id;
-	$Name=_DBEscString($Name);
-	$Opisanie=_DBEscString($Opisanie);
-	$Rabota=(int)($Rabota);
-	$Cena=(int)$Cena;
-	
-	//Выполнение запроса	
-	_DBQuery("
+    //Предотвращение SQL-инъекций
+    $id=(int)$id;
+    $Name=_DBEscString($Name);
+    $Opisanie=_DBEscString($Opisanie);
+    $Rabota=(int)($Rabota);
+    $Cena=(int)$Cena;
+
+    //Выполнение запроса
+    _DBQuery("
 		UPDATE vacancy 
 		SET	Nazvanie='$Name',
 		salary='$Cena',
@@ -78,10 +108,10 @@ function DBUpdateVacancy($id,$Name,$Cena,$Opisanie,$Rabota) {
 
 //Удаление элемента (Delete)
 function DBDeleteVacancy($id) {
-	//Предотвращение SQL-инъекций
-	$id=(int)$id;
-	
-	//Выполнение запроса
-	_DBQuery("DELETE FROM vacancy WHERE id=$id");
+    //Предотвращение SQL-инъекций
+    $id=(int)$id;
+
+    //Выполнение запроса
+    _DBQuery("DELETE FROM vacancy WHERE id=$id");
 }
 
--- www/rest.api.php
+++ www/rest.api.php
@@ -1,62 +1,64 @@
-<? 
- require_once("$_SERVER[DOCUMENT_ROOT]/../includes/flight/Flight.php");
- require_once("$_SERVER[DOCUMENT_ROOT]/../db/dal.inc.php");
-  
- function CreateVacancy() {
-	 DBCreateVacancy(
-		Flight::request()->data["Naizvanie"],
-		Flight::request()->data["Cena"],
-		Flight::request()->data["Opisanie"],
-		Flight::request()->data["rabota"]
-	);
- }
- Flight::route('PUT /rest/vacancy',"CreateVacancy");
- 
- function ReadVacancyOne($id) {
-	Flight::json(DBGetVacancyOne($id));
- }
- Flight::route('GET /rest/vacancy_one\?id=@id',"ReadVacancyOne");
- 
- function ReadJob_Openings() {
-	$data=Array();
+<?
+require_once("$_SERVER[DOCUMENT_ROOT]/../includes/flight/Flight.php");
+require_once("$_SERVER[DOCUMENT_ROOT]/../db/dal.inc.php");
 
-	while($row=DBFetchVacancy(
-		$_POST["search"]["value"],
-		$_POST['order']['0']['column'],
-		$_POST['order']['0']['dir'],
-		$_POST['start'],$_POST["length"])) 
-	{
-		$data[]=Array($row["Nazvanie"],$row["salary"],
-		'<button type="button" name="update" id="'.$row["ID"].'" class="btn btn-warning btn-xs update">Редактировать</button>',
-		'<button type="button" name="delete" id="'.$row["ID"].'" class="btn btn-danger btn-xs delete">Удалить</button>');	
-	}
+function CreateVacancy() {
+    DBCreateVacancy(
+        Flight::request()->data["Naizvanie"],
+        Flight::request()->data["Cena"],
+        Flight::request()->data["Opisanie"],
+        Flight::request()->data["rabota"]
+    );
+}
+Flight::route('PUT /rest/vacancy',"CreateVacancy");
 
+function ReadVacancyOne($id) {
+    Flight::json(DBGetVacancyOne($id));
+}
+Flight::route('GET /rest/vacancy_one\?id=@id',"ReadVacancyOne");
 
-	//Отправка данных клиенту в формате JSON (JavaScript Object Notation)
-	Flight::json(Array(
-			"draw"				=>	intval($_POST["draw"]),
-			"recordsTotal"		=> 	count($data),
-			"recordsFiltered"	=>	DBCountAllVacancy(),
-			"data"				=>	$data
-	));
-	
- }
- Flight::route('POST /rest/job_openings',"ReadJob_Openings");
- 
- function UpdateVacancy() {
-	 DBUpdateVacancy(
-		Flight::request()->data["ID"],
-		Flight::request()->data["Naizvanie"],
-		Flight::request()->data["Cena"],
-		Flight::request()->data["Opisanie"],
-		Flight::request()->data["rabota"]
-	);
- }
- Flight::route('PATCH /rest/vacancy',"UpdateVacancy");
- 
- function DeleteVacancy($id) {
-	 DBDeleteVacancy($id);
- }
- Flight::route('DELETE /rest/vacancy\?id=@id',"DeleteVacancy"); 
+function ReadJob_Openings() {
+    $data=Array();
 
- Flight::start();
+    while($row=DBFetchVacancy(
+        $_POST["name_search"],
+        $_POST['order']['0']['column'],
+        $_POST['order']['0']['dir'],
+        $_POST['start'],
+        $_POST["length"],
+        $_POST["sal_from_search"],
+        $_POST["sal_to_search"]
+    )){
+        $data[]=Array($row["Nazvanie"], $row["salary"],
+            '<button type="button" name="update" id="'.$row["ID"].'" class="btn btn-warning btn-xs update">Редактировать</button>',
+            '<button type="button" name="delete" id="'.$row["ID"].'" class="btn btn-danger btn-xs delete">Удалить</button>');
+    }
+
+    //Отправка данных клиенту в формате JSON (JavaScript Object Notation)
+    Flight::json(Array(
+        "draw"				=>	intval($_POST["draw"]),
+        "recordsTotal"		=> 	count($data),
+        "recordsFiltered"	=>	DBCountAllVacancy(),
+        "data"				=>	$data
+    ));
+
+}
+Flight::route('POST /rest/job_openings',"ReadJob_Openings");
+
+function UpdateVacancy() {
+    DBUpdateVacancy(
+        Flight::request()->data["ID"],
+        Flight::request()->data["Naizvanie"],
+        Flight::request()->data["Cena"],
+        Flight::request()->data["Opisanie"],
+        Flight::request()->data["rabota"]
+    );
+}
+Flight::route('PATCH /rest/vacancy',"UpdateVacancy");
+
+function DeleteVacancy($id) {
+    DBDeleteVacancy($id);
+}
+Flight::route('DELETE /rest/vacancy\?id=@id',"DeleteVacancy");
+
+Flight::start();
